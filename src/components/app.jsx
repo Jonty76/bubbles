3df894115b609c0a4b794d5e6f0460e5b7774308
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2015-12-07 17:35:16 +0000

    as in #29, #23. Created basic test enviroment. fix #29

---
+++ src/components/app.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1> hello world</h1>
+      </div>
+    );
+  }
+});
+
+module.exports = App;
