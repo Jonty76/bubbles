3cd70ea9a239594c7a07e75711b0ccb306f8327e
thegsi <thegsi@hotmail.com>
2015-12-09 15:29:43 +0000

    calendar added flight details page state working #48 #12

--- src/js/components/views/select-flight.jsx
+++ src/js/components/views/select-flight.jsx
@@ -1,50 +1,78 @@
 import React from 'react';
 import Formsy from 'formsy-react';
-import { Select } from 'formsy-react-components';
-import {Router, Route, Link} from 'react-router';
+import {Select} from 'formsy-react-components';
+// import {Router, Route, Link} from 'react-router';
 import Calendar from 'react-input-calendar';
 
-let airlineSelectorClass = React.createClass({
+let Selector = React.createClass({
   render: function() {
     let airlineOptions = [
-      {label: "British Airways"},
-      {label: "Emirates"},
-      {label: "Air Japan"},
-      {label: "South African Airways"}
+      {
+        label: "British Airways"
+      }, {
+        label: "Emirates"
+      }, {
+        label: "Air Japan"
+      }, {
+        label: "South African Airways"
+      }
     ];
 
     return (
-      <Select
-        name="airlineSelector"
-        options={airlineOptions}
-      />
+      <Select name="airlineSelector" options={airlineOptions} onChange={this.props.onChange}/>
     );
   }
 });
 
-let calendarSelectorClass = React.createClass({
-  render: function (){
+
+let Calendar2 = React.createClass({
+  render: function() {
     return (
-      <Calendar
-        format = "DD/MM/YYYY"
-        date="08-12-2015"
-      />,
-    document.body
-    );
-  }
+      <Calendar date="12-09-2015" format="DD/MM/YYYY" openOnInputFocus="bool" closeOnSelect="bool" minView="0" minView="1"/>
 
+  )}
 });
+
+let FlightDetails = React.createClass({
+  render: function(){
+    return (
+      <p> This is your airline: {this.props.airline} </p>
+    )
+  }
+});
+
 
 let Page = React.createClass({
-  render:function(){
+  getInitialState: function(){
+    return {
+      userAirline: ""
+    };
+  },
+
+  selectAirline: function(airline){
+    this.setState({
+      userAirline: airline
+    });
+  },
+
+  selectorChange: function(name, value){
+    this.selectAirline(value);
+  },
+
+  render: function() {
     return (
       <div>
         <Formsy.Form>
           <fieldset>
-            <airlineSelectorClass />
+            <Selector onChange= {this.selectorChange} />
           </fieldset>
         </Formsy.Form>
-        <calendarSelectorClass />
+        <Calendar2 />
+        <Formsy.Form>
+        <input type='text' placeholder="Flight number" />
+        <input class="btn btn-primary" formnovalidate="" type="submit" value="Go" />
+        </Formsy.Form>
+        <FlightDetails airline={this.state.userAirline} />
       </div>
     );
   }
@@ -52,8 +80,8 @@ let Page = React.createClass({
 
 module.exports = ({
   Page,
-  Components:{
-    airlineSelectorClass,
-    calendarSelectorClass
+  Components: {
+    Selector,
+    Calendar2
   }
 });
