6c064f1e3dd9606b0f497ad39ad2e39290b473f2
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2015-12-10 12:14:15 +0000

    adding CVV input box to payment page #68

--- src/js/components/views/pay.jsx
+++ src/js/components/views/pay.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Formsy from 'formsy-react';
-import { Select } from 'formsy-react-components';
+import { Select, Input } from 'formsy-react-components';
 import { Link } from 'react-router';
 
 let Selector = React.createClass({
  render: function() {
    let cardOptions = [
      {label : "Choose card"},
-     {label : "Placeholder for card data"} 
+     {label : "Placeholder for card data"}
    ];
    return (
      <Formsy.Form>
@@ -42,9 +42,18 @@ let Page = React.createClass({
         {!this.state.cardSelected ? (
           <p>Please choose a card</p>
         ) : (
+          <Formsy.Form>
+          <Input
+            name="cvv"
+            value=""
+            type="text"
+            label="CVV"
+            required
+            />
           <Link to="/order-confirmation">
             <button>PAY</button>
           </Link>
+          </Formsy.Form>
         )}
       </div>
     );
