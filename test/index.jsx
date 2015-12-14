7638368424732c8f9c7f7eeff46ea1bc9eb5ff02
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2015-12-14 14:50:23 +0000

    adding tests for components #94

--- test/index.jsx
+++ test/index.jsx
@@ -2,4 +2,8 @@ require('./app.test.jsx');
 require('./select-airport.test.jsx');
 require('./hapi-test.js');
 require('./header.test.jsx');
-require('./select-flight.test.jsx')
+require('./select-flight.test.jsx');
+// require('./food-item.test.jsx');
+// require('./food-type.test.jsx');
+// require('./menu.test.jsx');
+// require('./restaurant-header.test.jsx');
