384ab57e0f148377923714310ccc9389cbd36b58
Naaz <marziyeh.naaz.ahmed@gmail.com>
2015-12-18 19:42:14 +0000

    more styling #27

--- src/js/components/views/menu.jsx
+++ src/js/components/views/menu.jsx
@@ -24,7 +24,7 @@ var Menu = React.createClass({
 
     var yoSushiImage= (
     <div>
-      <img className = "restaurant-logo"
+      <img className = "restaurant-logo-menu"
       src = "https://cloud.githubusercontent.com/assets/11833296/11876966/475301e4-a4e4-11e5-8207-92838be02f37.jpg"/>
       <h1 className = "restaurant-name-on-menu">{this.props.tagValue}</h1>
       <h4 className = "restaurant-description-on-menu">{this.props.helpers.getDescription(this.props.tagValue)}</h4>
@@ -33,7 +33,7 @@ var Menu = React.createClass({
 
     var pretImage = (
       <div>
-        <img className = "restaurant-logo"
+        <img className = "restaurant-logo-menu"
         src = "https://cloud.githubusercontent.com/assets/11833296/11877105/161c52a0-a4e5-11e5-94b4-8217e73260a6.jpg"/>
         <h1 className = "restaurant-name-on-menu">{this.props.tagValue}</h1>
         <h4 className = "restaurant-description-on-menu">{this.props.helpers.getDescription(this.props.tagValue)}</h4>
@@ -42,7 +42,7 @@ var Menu = React.createClass({
 
     var grainStoreImage = (
       <div>
-      <img className = "restaurant-logo"
+      <img className = "restaurant-logo-menu"
         src = "https://cloud.githubusercontent.com/assets/11833296/11897909/719aeb9a-a58b-11e5-8680-99488d69bef1.jpeg"/>
         <h1 className = "restaurant-name-on-menu">{this.props.tagValue}</h1>
         <h4 className = "restaurant-description-on-menu">{this.props.helpers.getDescription(this.props.tagValue)}</h4>
