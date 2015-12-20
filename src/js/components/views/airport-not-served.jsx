167c49c09f25540c87ef8efa9f28aace0fc67f20
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2015-12-20 21:26:03 +0000

    changing the filler text on all relevant pages #166

--- src/js/components/views/airport-not-served.jsx
+++ src/js/components/views/airport-not-served.jsx
@@ -19,8 +19,20 @@ let Page = React.createClass({
   },
 
   render: function() {
+    var sidePadding = {
+      paddingLeft: '10em'
+    };
+    var fontSize = {
+      fontSize: '1em'
+    };
+    var smallerFont = {
+      fontSize: '1.1em'
+    };
     var submittedText = (
-      <p className = "view-text">Thank You!</p>
+      <div>
+        <p className = "view-text" style={fontSize}>Thank you for adding your voice to the call for Piccnicc to be introduced to Heathrow Airport!</p>
+        <p className='view-text' style={fontSize}> We'll update you as soon as we have something newsworthy - in the meantime, safe travels and by all means follow us on <a href='https://twitter.com/piccniccapp'>twitter</a> or contact us anytime through our <a href='http://www.piccnicc.com/'>website.</a></p>
+      </div>
     );
     var submitForm = (
       <Formsy.Form onSubmit={this.submitForm}>
@@ -33,20 +45,27 @@ let Page = React.createClass({
             }}
             />
         </div>
-        <input className="next-button"
-          type="submit"
-          defaultValue="SUBMIT"
-          />
+        <div style={sidePadding}>
+          <input
+            type="submit"
+            defaultValue="SUBMIT"
+            />
+        </div>
       </Formsy.Form>
     );
     return (
       <div>
 
-        <p className='view-text'>
-          Sorry we do not currently serve this airport.
-          Enter your email and we'll update you when we do!
+        <p className='view-text' style={smallerFont}>
+          Sorry, you can't get Piccnicc at Heathrow Airport yet.
+          However, the more demand we can show for the service, the sooner we can launch it there.
+          Please provide your email address below so we can add your support and update you when the time comes.
          </p>
          {this.state.emailSubmitted ? submittedText : submitForm}
+         <p style={fontSize} className='view-text'>Piccnicc - Hampers of Happiness, Delivered</p>
+         <p style={fontSize} className='view-text'>Visit us at <a href='http://www.piccnicc.com/'>piccnicc.com</a></p>
+         <p style={fontSize} className='view-text'>Follow us on <a href='https://twitter.com/piccniccapp'>twitter.com/piccniccapp</a> #nomoregreychicken</p>
+         <p style={fontSize} className='view-text'>Privacy Policy: We promise never to share your email or any other details with anybody else, ever. Nobody. Never.</p>
       </div>
     );
   }
