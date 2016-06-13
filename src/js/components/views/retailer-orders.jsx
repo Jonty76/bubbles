f8ad57692c6ce8f852f82207670cbcf65fa5fb98
Katerina <katerina.pascoulis@gmail.com>
2016-06-13 16:37:51 +0100

    Order collection added

--- src/js/components/views/retailer-orders.jsx
+++ src/js/components/views/retailer-orders.jsx
@@ -11,6 +11,20 @@ let RetailerOrders = React.createClass({
       <div className="custom-container">
         <RedHeader text={"Orders"} iconRight={"error_outline"} iconLeft={"arrow_back"} />
 
+        <ul className="collapsible" data-collapsible="expandable">
+          <li>
+            <div className="collapsible-header">First<i className="material-icons right-align">check_circle</i></div>
+            <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+          </li>
+          <li>
+            <div className="collapsible-header"><i className="material-icons">place</i>Second</div>
+            <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+          </li>
+          <li>
+            <div className="collapsible-header"><i className="material-icons">whatshot</i>Third</div>
+            <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+          </li>
+        </ul>
 
       </div>
     )
