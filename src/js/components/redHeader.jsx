b783957017fcdb66c6fa91691855526149e73597
Ruth <emailtoruth@virginmedia.com>
2016-06-13 11:44:52 +0100

    changed classnames of icons on red header

--- src/js/components/redHeader.jsx
+++ src/js/components/redHeader.jsx
@@ -10,8 +10,8 @@ var Header = React.createClass({
           <div className="header nav-wrapper red-nav">
             <p className="brand-logo center white-text" id="brand-logo">{this.props.text}</p>
               <ul>
-                <li className="right"><i className="menu-icon material-icons white-text">done</i></li>
-                <li className="left"><i className="back-icon material-icons white-text">settings</i></li>
+                <li className="right"><i className="done-icon material-icons white-text">done</i></li>
+                <li className="left"><i className="settings-icon material-icons white-text">settings</i></li>
               </ul>
           </div>
         </nav>
