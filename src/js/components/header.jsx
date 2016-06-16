b2642abc8b6642d6e5aef3a2e43277138f54fe0c
Ruth <emailtoruth@virginmedia.com>
2016-06-16 13:15:17 +0100

    burger menu wip #238

--- src/js/components/header.jsx
+++ src/js/components/header.jsx
@@ -3,6 +3,7 @@ import { Link, hashHistory } from 'react-router';
 
 var Header = React.createClass({
   componentDidMount: function (){
+
     if(this.props.iconLeft === "error_outline") {
       document.getElementById('icon-left').classList.add("modal-trigger")
 
@@ -26,15 +27,23 @@ var Header = React.createClass({
         <nav>
           <div id="header" className="header nav-wrapper">
             <p className="brand-logo center" id="brand-logo">{this.props.text}</p>
-              <ul>
-                <li className="left"><i id="icon-left" className="icon-left material-icons">{this.props.iconLeft}</i></li>
-                <li className="right"><i id="icon-right" className="icon-right material-icons">{this.props.iconRight}</i></li>
-              </ul>
+              <p data-activates="mobile-demo" className="button-collapse right"><i id="icon-right" className="material-icons icon-right">menu</i></p>
+                <ul className="side-nav" id="mobile-demo">
+                  <li><Link to="/about">About</Link></li>
+                  <li><Link to="/order-history">Order History</Link></li>
+                  <li><Link to='/login'>Logout</Link></li>
+                </ul>
+                <p className="left"><i id="icon-left" className="material-icons icon-left">{this.props.iconLeft}</i></p>
           </div>
         </nav>
       </div>
     );
   }
 });
+
+// <ul>
+//   <li className="left"><i id="icon-left" className="icon-left material-icons">{this.props.iconLeft}</i></li>
+//   <li className="right"><i id="icon-right" className="icon-right material-icons">{this.props.iconRight}</i></li>
+// </ul>
 
 module.exports = Header;
