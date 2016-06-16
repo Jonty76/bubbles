9253be90b59076fba4ba97be73e0b7f81ae19d99
Ruth <emailtoruth@virginmedia.com>
2016-06-16 14:27:18 +0100

    burger menu functionality done, some styling still needed to be done #238

--- src/js/components/header.jsx
+++ src/js/components/header.jsx
@@ -3,6 +3,13 @@ import { Link, hashHistory } from 'react-router';
 
 var Header = React.createClass({
   componentDidMount: function (){
+    $(document).ready(function(){
+      $("#button-collapse").sideNav({
+        menuWidth: 180,
+        edge: 'right',
+        closeOnClick: true
+      });
+    })
 
     if(this.props.iconLeft === "error_outline") {
       document.getElementById('icon-left').classList.add("modal-trigger")
@@ -24,26 +31,22 @@ var Header = React.createClass({
   render: function() {
     return (
       <div className="navbar-fixed">
-        <nav>
+        <nav className="nav-class">
           <div id="header" className="header nav-wrapper">
             <p className="brand-logo center" id="brand-logo">{this.props.text}</p>
-              <p data-activates="mobile-demo" className="button-collapse right"><i id="icon-right" className="material-icons icon-right">menu</i></p>
-                <ul className="side-nav" id="mobile-demo">
-                  <li><Link to="/about">About</Link></li>
-                  <li><Link to="/order-history">Order History</Link></li>
-                  <li><Link to='/login'>Logout</Link></li>
+              <ul data-activates="mobile-demo" id="button-collapse" className="button-collapse right"><i id="icon-right" className="material-icons icon-right">menu</i></ul>
+                <ul className="side-nav fixed" id="mobile-demo">
+                  <li>Hidden</li>
+                  <li><Link className="burger-menu-item right" to="/about">About</Link></li>
+                  <li><Link className="burger-menu-item right" to="/order-history">Order History</Link></li>
+                  <li><Link className="burger-menu-item right" to='/login'>Logout</Link></li>
                 </ul>
-                <p className="left"><i id="icon-left" className="material-icons icon-left">{this.props.iconLeft}</i></p>
+                <ul className="left"><i id="icon-left" className="material-icons icon-left">{this.props.iconLeft}</i></ul>
           </div>
         </nav>
       </div>
     );
   }
 });
-
-// <ul>
-//   <li className="left"><i id="icon-left" className="icon-left material-icons">{this.props.iconLeft}</i></li>
-//   <li className="right"><i id="icon-right" className="icon-right material-icons">{this.props.iconRight}</i></li>
-// </ul>
 
 module.exports = Header;
