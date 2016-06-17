cf770c23614d8da034a5d818f61350329a0b3775
Ruth <emailtoruth@virginmedia.com>
2016-06-17 12:30:02 +0100

    order details functionality done #207

--- src/js/components/menu.jsx
+++ src/js/components/menu.jsx
@@ -2,26 +2,6 @@ import React from 'react';
 import { FoodType } from './food-type.jsx';
 import { Link } from 'react-router';
 
-let ChooseMore = React.createClass({
-  render: function() {
-    var chooseMoreStyling = {
-      marginBottom: '4em'
-    };
-    return this.props.show ? (
-      <Link to='/basket/select-menu'>
-        <div
-          style={chooseMoreStyling}
-          className="order-more-link pull-left"
-        >
-          <h4>Order More</h4>
-        </div>
-      </Link>
-    ) : (
-      <div></div>
-    );
-  }
-});
-
 let Menu = React.createClass({
   render: function() {
     return (
@@ -33,7 +13,6 @@ let Menu = React.createClass({
             </div>
           );
         }.bind(this))}
-        <ChooseMore show={this.props.showBackLink} />
       </div>
     );
   }
--- src/js/components/views/order-details.jsx
+++ src/js/components/views/order-details.jsx
@@ -2,81 +2,30 @@ import React from 'react';
 import { Link } from 'react-router';
 import Header from '../header.jsx';
 
-var activeOrders = require('./order-confirmation.jsx').activeOrder;
-
-let GetOrder = React.createClass({
-  componentDidUpdate: function (nextProps, nextState){
-
-  },
-
-  renderLineItem: function (details) {
-    return details.map(function(lineItem){
-      return (
-        <div className="row">
-          <div className="retailer-quantity col s2 m2 left-align"><span>{lineItem.quantityOrdered}</span></div>
-          <div className="retailer-line-item col s4 m4"><span>{lineItem.name}</span></div>
-          <div className="retailer-price col s1 m1 right-align"><span>{lineItem.price}</span></div>
-        </div>
-      )
-    })
-  },
-
-  renderOrderByRestaurant: function(order){
-    var that = this
-    return order.map(function(elem){
-      var items = elem.items
-      return (
-        <div>
-          <h3>{elem.name}</h3>
-          {that.renderLineItem(items)}
-        </div>
-      )
-    })
-  },
-
-  render: function() {
-    if (activeOrders.length === 0) {
-      return (
-        <div className="white-background">
-        <div className="divider"></div>
-          <div className="order-option">
-            <h4 className="red-color">No active orders</h4>
-          </div>
-        <div className="divider"></div>
-      </div>
-    )
-  } else {
-    var order = activeOrders[0]
-    return (
-    <div>
-      <h1>Order</h1>
-      {this.renderOrderByRestaurant(order)}
-    </div>
-    )
-  }
-}
-})
-
+let MenuComponent = require('../menu.jsx')
 
 let OrderDetails = React.createClass({
-  emptyArray: function(){
-    activeOrders = [];
-  },
 
-  onClick: function (){
-    this.props.actions.clearBasket()
-    this.emptyArray()
+  getCheckoutList: function() {
+    var wholeMenu = this.props.basket;
+    var filterer = this.props.helpers.filterMenu;
+    var quantityFilterer = this.props.helpers.filterMenuByQuantity;
+    var structurer = this.props.helpers.orderMenu;
+    var quantityFilteredMenu = quantityFilterer(wholeMenu);
+    return structurer(quantityFilteredMenu, "restaurant");
   },
 
   render: function() {
     var burgerMenuOptions = ["About+/about", "Create Order+/", "Order History+/order-history", "Logout+/login"]
+    var menu = this.getCheckoutList();
 
     return (
       <div className="grey-background">
         <Header headerTheme={"whiteNav"} text={"Order Details"} iconRight={"menu"} iconLeft={"arrow_back"} burgerMenuOptions={burgerMenuOptions}/>
-        <GetOrder />
-        <button>Edit Order</button>
-        <button onClick={this.onClick}>Cancel Order</button>
+
+        <MenuComponent menu={menu} actions={this.props.actions} inCheckout={true} />
+        <Link to='/basket/select-restaurant'><button>Edit Order</button></Link>
+        <button onClick={this.props.actions.clearBasket}>Cancel Order</button>
     </div>
     );
   }
