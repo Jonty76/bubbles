f94864357004530c689c3eca20e620f6b5ab518c
Katerina <katerina.pascoulis@gmail.com>
2016-06-19 21:55:07 +0100

    Changed class to classname, and added else which displays both #213

--- src/js/components/views/map-view.jsx
+++ src/js/components/views/map-view.jsx
@@ -14,15 +14,19 @@ var MapView = React.createClass({
     var terminal = this.props.terminal
     if (terminal === "north") {
       return (
-        <img class="materialboxed" width="400" src="https://cloud.githubusercontent.com/assets/9627463/16157776/fabdf74a-34b1-11e6-90cb-01627b05bffd.png"></img>
+        <img className="materialboxed" width="400" src="https://cloud.githubusercontent.com/assets/9627463/16157776/fabdf74a-34b1-11e6-90cb-01627b05bffd.png"></img>
       )
     } else if (terminal === "south"){
       return (
-        <img class="materialboxed" width="400" src="https://cloud.githubusercontent.com/assets/9627463/16157777/fabe7292-34b1-11e6-91fa-732656c75d0c.png"></img>
+        <img className="materialboxed" width="400" src="https://cloud.githubusercontent.com/assets/9627463/16157777/fabe7292-34b1-11e6-91fa-732656c75d0c.png"></img>
       )
     } else {
       return (
-        <div className="error-message">No Terminal Selected. Did you select your airline?</div>
+        <div>
+          <img className="materialboxed" width="400" src="https://cloud.githubusercontent.com/assets/9627463/16157776/fabdf74a-34b1-11e6-90cb-01627b05bffd.png"></img>
+          <div className="divider"></div>
+          <img className="materialboxed" width="400" src="https://cloud.githubusercontent.com/assets/9627463/16157777/fabe7292-34b1-11e6-91fa-732656c75d0c.png"></img>
+        </div>
       )
    }
   },
