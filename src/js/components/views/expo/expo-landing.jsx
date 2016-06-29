21972406d3661a8bbc5bead7d0f8102a8449ca24
Katerina <katerina.pascoulis@gmail.com>
2016-06-29 16:07:30 +0100

    #354 Created Expo data, rendered select expo and select conf fields so dynamically load from DB

--- src/js/components/views/expo/expo-landing.jsx
+++ src/js/components/views/expo/expo-landing.jsx
@@ -1,6 +1,74 @@
 import React from 'react';
 import Header from '../../header.jsx';
 import { Link } from 'react-router';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+
+let expoData  = require('../../../data/expo-data.js');
+
+
+let ExpoDetails = React.createClass({
+  getInitialState: function() {
+    return {
+      selectedExpoCentre: "",
+      selectedExpo: ""
+    };
+  },
+
+  selectorChange(keyName, event, index, value){
+      var change = {};
+      change[keyName] = value;
+      this.setState(change);
+  },
+
+  renderExpo: function() {
+    var selectedExpoCentre = this.state.selectedExpoCentre
+    var filteredExpos = Object.keys(expoData[selectedExpoCentre])
+    return filteredExpos.map(function(expo){
+      var name = expoData[selectedExpoCentre][expo].name
+
+      return (
+        <MenuItem value={expo} primaryText={name} key={name}/>
+      )
+    })
+  },
+
+  selectExpo: function() {
+    if (this.state.selectedExpoCentre !== "") {
+      return (
+        <div>
+        <div>Select Expo</div>
+          <SelectField className="dropdown" value={this.state.selectedExpo} floatingLabelText="Select Exhibition Centre" onChange={this.selectorChange.bind(this, 'selectedExpo')}>
+            {this.renderExpo()}
+          </SelectField >
+        </div>
+      )
+    }
+  },
+
+  renderExpoCentre: function() {
+    return Object.keys(expoData).map(function(venue){
+      return (
+        <MenuItem value={venue} primaryText={venue} key={venue}/>
+      )
+    })
+  },
+
+  render: function() {
+    return (
+      <div className="center-align">
+        <SelectField className="dropdown" value={this.state.selectedExpoCentre} floatingLabelText="Select Exhibition Centre" onChange={this.selectorChange.bind(this, 'selectedExpoCentre')}>
+          {this.renderExpoCentre()}
+        </SelectField >
+
+        {this.selectExpo()}
+
+      </div>
+
+    );
+  }
+})
+
 
 var ExpoLanding = React.createClass({
   render: function() {
@@ -11,7 +79,13 @@ var ExpoLanding = React.createClass({
       <div>
         <Header headerTheme={"whiteNav"} text={"Piccnicc"} iconRight={"menu"} iconLeft={""} burgerMenuOptions={burgerMenuOptions}/>
         <div className="center-align desktop-container">
-          Hello World
+          <div className="content-container">
+            <div className="question-container">
+              <p className="standard-question-style">Hampers of Happiness, Delivered </p>
+            </div>
+
+            <ExpoDetails />
+          </div>
         </div>
       </div>
     );
---
+++ src/js/data/expo-data.js
@@ -0,0 +1,32 @@
+var expoData = {
+    Excel: {
+      HealthConf: {
+        code: "HealthConf",
+        name: "Health Conference 2016 - 20th & 21st Aug",
+        venue: "Excel Centre",
+        dates: [1471734000000, 1471820400000]
+      },
+      MoneyConf: {
+        code: "MoneyConf",
+        name: "Money Conference 2016 - 31st Aug & 1st Sept",
+        location: "Excel Centre",
+        dates: [1472684400000, 1472770800000]
+      }
+    },
+    Olympia: {
+      TechConf: {
+        code: "TechConf",
+        name: "Tech Conference 2016 - 7th & 8th Sept",
+        location: "Olympia",
+        dates: [1473289200000, 1473375600000]
+      },
+      SecurityConf: {
+        code: "SecurityConf",
+        name: "Security Conference 2016 - 19th & 20th Sept",
+        location: "Olympia",
+        dates: [1474326000000, 1474412400000]
+      }
+    }
+};
+
+module.exports = expoData;
