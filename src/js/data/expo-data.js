8ec426e8a2c223c3b1d3ee4f26b918ed21b4fd29
Katerina <katerina.pascoulis@gmail.com>
2016-06-30 13:34:56 +0100

    #382 Changed dates in data to match dates in other part of object

--- src/js/data/expo-data.js
+++ src/js/data/expo-data.js
@@ -2,13 +2,13 @@ var expoData = {
     Excel: {
       HealthConf: {
         code: "HealthConf",
-        name: "Health Conference '16: 20th & 21st Aug",
+        name: "Health Conference '16: 21st & 22nd Aug",
         venue: "Excel Centre",
         dates: [1471734000000, 1471820400000]
       },
       MoneyConf: {
         code: "MoneyConf",
-        name: "Money Conference '16: 31st Aug & 1st Sept",
+        name: "Money Conference '16: 1st & 2nd Sept",
         location: "Excel Centre",
         dates: [1472684400000, 1472770800000]
       }
@@ -16,13 +16,13 @@ var expoData = {
     Olympia: {
       TechConf: {
         code: "TechConf",
-        name: "Tech Conference '16: 7th & 8th Sept",
+        name: "Tech Conference '16: 8th & 9th Sept",
         location: "Olympia",
         dates: [1473289200000, 1473375600000]
       },
       SecurityConf: {
         code: "SecurityConf",
-        name: "Security Conference '16: 19th & 20th Sept",
+        name: "Security Conference '16: 20th & 21st Sept",
         location: "Olympia",
         dates: [1474326000000, 1474412400000]
       }
