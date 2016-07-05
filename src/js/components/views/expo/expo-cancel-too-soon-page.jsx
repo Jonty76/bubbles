b3a65efe5a3cb1a2fa6217b756b365a8b9e92838
Ruth <emailtoruth@virginmedia.com>
2016-07-05 12:48:56 +0100

    cancel order flow completed #368. @katpas need to hook up link into emails

---
+++ src/js/components/views/expo/expo-cancel-too-soon-page.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router';
+import Header from '../../header.jsx';
+
+var ExpoOrderConfirmedPage = React.createClass({
+
+  render: function() {
+
+    var burgerMenuOptions = ["About+/about", "Create Order+/", "Piccnicc Point+/map-view", "Order History+/order-history", "Logout+/login"]
+
+    return (
+    <div className="custom-container desktop-container">
+      <Header headerTheme={"whiteNav"} text={"Cancel Order"} iconRight={"menu"} iconLeft={""} burgerMenuOptions={burgerMenuOptions}/>
+      <div className="order-confirmed-container center-align">
+
+        <p id="top-margin">Your order cannot be cancelled less than 45 minutes before your delivery time.</p>
+
+        <div className="">
+          <img className="logo-container" src="/piccnicclogo.png" alt="Piccnicc Logo"></img>
+        </div>
+
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = ExpoOrderConfirmedPage
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -29,6 +29,9 @@ let ExpoLanding           = require('./components/views/expo/expo-landing.jsx');
 let ExpoPayment           = require('./components/views/expo/expo-payment.jsx');
 let ExpoFaq               = require('./components/views/expo/expo-faq.jsx');
 let ExpoOrderConfirmed    = require('./components/views/expo/expo-order-confirmed-page.jsx')
+let ExpoCancelOrder       = require('./components/views/expo/expo-cancel-order-page.jsx')
+let ExpoTooSoon           = require('./components/views/expo/expo-cancel-too-soon-page.jsx')
+
 
 injectTapEventPlugin();
 
@@ -53,6 +56,8 @@ render((
       <Route path="/map-view" component={MapView} />
       <Route path="/order-confirmation" component={OrderConfirmation} />
       <Route path="expo-order-confirmed-page" component={ExpoOrderConfirmed} />
+      <Route path="expo-cancel-order-page" component={ExpoCancelOrder} />
+      <Route path="expo-cancel-too-soon-page" component={ExpoTooSoon} />
       <Route path="/order-details" component={OrderDetails} />
       <Route path="/order-track" component={OrderTrack} />
       <Route path="/order-track-complete" component={OrderTrackComplete} />
