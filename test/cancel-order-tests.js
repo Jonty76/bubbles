bbb0fc5025a3a79d2a9ccda29a415e9c60512eb7
Ruth <emailtoruth@virginmedia.com>
2016-07-07 12:21:59 +0100

    cancel flow completed #368

--- test/cancel-order-tests.js
+++ test/cancel-order-tests.js
@@ -2,28 +2,13 @@ var test   = require('tape');
 var start = require('../lib/start.js');
 var server = require("../lib/index.js");
 
-test("does too soon page show if user tries to cancel order less than 45 minutes before delivery time", function(t) {
-  var timeNow = new Date().getTime()
-  var lessThan45 = timeNow + 30*60000
-  var options = {
-    method: "GET",
-    url: "/cancel-order/ORDER-NUMBER/" + lessThan45
-  };
-  server.inject(options, function(response) {
-    t.equal(response.headers.location, "/#/expo-cancel-too-soon-page", "Too soon page should be returned")
-    t.end()
-  });
-});
-
-test("does user get redirected to cancelled order page successfully", function(t) {
-  var timeNow = new Date().getTime()
-  var greaterThan45 = timeNow + 60*60000
-  var options = {
-    method: "GET",
-    url: "/cancel-order/ORDER-NUMBER/" + greaterThan45
-  };
-  server.inject(options, function(response) {
-    t.equal(response.headers.location, "/#/expo-cancel-order-page", "Cancel order page should be returned")
-    t.end()
-  });
+test("Is cancel route working correctly", function(t) {
+    var options = {
+      method: "POST",
+      url: "/cancel-order/ORDER-NUMBER"
+    };
+    server.inject(options, function(response) {
+      t.equal(response.payload, "cancel email sent", "Cancel route working correctly");
+      t.end()
+    });
 });
